1df7a4e158b5c9858df6e64033adb94ccdfe8148
Dmytro Zakharchuk <dmytro.zakharchuk@pm.bet>
2020-10-20 23:42:20 +0300

    feat: add vuetify and example of context menu for badge

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
-  <div id="app">
+  <v-app>
     <text-annotator :data="sample2" />
-  </div>
+    <div class="text-center">
+      <v-badge
+        @click.native="onBadgeClick"
+      >
+        <span >
+          Item One
+        </span>
+      </v-badge>
+
+      <v-menu
+        v-model="showMenu"
+        :position-x="position.x"
+        :position-y="position.y"
+        offset-y
+        absolute
+      >
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+          >
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -17,10 +45,29 @@ export default {
   data() {
     return {
       sample2,
+      showMenu: false,
+      position: {
+        x: 0,
+        y: 0,
+      },
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
     };
+  },
+  methods: {
+    onBadgeClick(e) {
+      console.log('click');
+      this.showMenu = false;
+      this.position.x = e.clientX;
+      this.position.y = e.clientY;
+      this.$nextTick(() => {
+        this.showMenu = true;
+      });
+    },
   },
 };
 </script>
-
-<style lang="scss">
-</style>
